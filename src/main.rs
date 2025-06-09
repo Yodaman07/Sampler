dbdbe266377b9d0b18af02dbb1145423258158a3
Yodaman07 <ayaan.irshad@gmail.com>
2025-06-08 17:46:58 -0700

    repeating parts of song

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 use std::fs::File;
 use std::io::BufReader;
-use rodio::{Decoder, OutputStream, source::Source};
+use std::time::Duration;
+use rodio::{Decoder, OutputStream, source::Source, Sink};
+use rodio::source::SineWave;
 
 fn main() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).expect("Couldn't make sink");
+
+
 
     //file path relative to Cargo.toml
-    let file = File::open("music/nikes.mp3").expect("Couldn't open file");
+    let file = File::open("music/ivy.mp3").expect("Couldn't open file");
     let file = BufReader::new(file);
 
     let source = Decoder::new(file).expect("Couldn't decode");
+    // let source = source.fade_in(Duration::from_secs(4)).take_duration(Duration::from_secs(2)).repeat_infinite();
+    let source = source.skip_duration(Duration::from_secs(100)).take_duration(Duration::from_secs(10));
+    
+    
+    
+    
+    sink.append(source);
 
-    stream_handle.play_raw(source.convert_samples()).expect("Unable to convert samples");
-
-    std::thread::sleep(std::time::Duration::from_secs(10));
+    sink.sleep_until_end();
     println!("Hello, world!");
 }
