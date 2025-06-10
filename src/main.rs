0334e0d898d4726b6a8f883809da619c209ddf7a
Yodaman07 <ayaan.irshad@gmail.com>
2025-06-09 19:19:23 -0700

    Can layer multiple songs easily

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,64 @@ use std::fs::File;
 use std::io::BufReader;
 use std::time::Duration;
 use rodio::{Decoder, OutputStream, source::Source, Sink};
+use rodio::source::{SkipDuration, Speed, TakeDuration};
 use youtube_dl::YoutubeDl;
 
-fn main() {
-    let video_title = download_file("https://www.youtube.com/watch?v=nLpTpr7_Ye8");
+struct Song{
+    path: String,
+    //properties
+    speed: f32,
+    start_time: f32,
+    end_time: f32,
+    loop_track: bool
+}
 
+impl Song {
+    fn create(&self) -> TakeDuration<SkipDuration<Speed<Decoder<BufReader<File>>>>> {
+        let file = File::open(&self.path).expect("Couldn't open file");
+        let file = BufReader::new(file);
+
+        let source = Decoder::new(file).expect("Couldn't decode");
+
+        let source = source
+            .speed(self.speed)
+            .skip_duration(Duration::from_secs_f32(self.start_time))
+            .take_duration(Duration::from_secs_f32(self.end_time-self.start_time));
+
+        source
+    }
+}
+
+
+fn main() {
+    // let video_title = download_file("https://www.youtube.com/watch?v=nLpTpr7_Ye8");
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).expect("Couldn't make sink");
+    //Sink is like the audio player
+
 
     //file path relative to Cargo.toml
-    let file = File::open(format!("music/{}.mp3", video_title)).expect("Couldn't open file");
-    // let file = File::open("music/dtmlive.mp3").expect("Couldn't open file");
-    let file = BufReader::new(file);
+    // let file = File::open(format!("music/{}.mp3", video_title)).expect("Couldn't open file");
 
-    let source = Decoder::new(file).expect("Couldn't decode");
+    //When I start on the sampler, I should have a song stuct but also have clip structs where I can save each clip 
+    let mut dtmlive: Song = Song{
+        path: String::from("music/dtmlive.mp3"),
+        speed: 1.3,
+        start_time: 0.0,
+        end_time: 10.0,
+        loop_track: false
+    };
+
     //messing with the song
     // let source = source.take_duration(Duration::from_secs_f32(0.25));
-    // let source = source.speed(1.2);
-
     // let source = source.fade_in(Duration::from_secs(4)).take_duration(Duration::from_secs(2)).repeat_infinite();
-    // let source = source.skip_duration(Duration::from_secs(100)).take_duration(Duration::from_secs(10));
-    sink.append(source);
+    
+    sink.append(dtmlive.create());
+
+    dtmlive.speed = 1.0;
+    dtmlive.start_time = 13.0;
+    dtmlive.end_time = 25.0;
+    sink.append(dtmlive.create());
 
     sink.sleep_until_end();
     println!("Hello, world!");
