3f2d050f7fbfbe49f50afa42bfdb6676f3f2c7e9
Yodaman07 <ayaan.irshad@gmail.com>
2025-06-12 22:38:07 -0700

    audio stuff is good for now - time for ui

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,26 @@
 use std::fs::File;
 use std::io::BufReader;
+use std::iter::Skip;
 use std::time::Duration;
-use rodio::{Decoder, OutputStream, source::Source, Sink};
+use rodio::{Decoder, OutputStream, source::Source, Sink, Sample};
+use rodio::cpal::FromSample;
 use rodio::source::{SkipDuration, Speed, TakeDuration};
 use youtube_dl::YoutubeDl;
 
-struct Song{
-    path: String,
-    //properties
-    speed: f32,
-    start_time: f32,
-    end_time: f32,
-    loop_track: bool
-}
+struct Song{ path: String }
 
 impl Song {
-    fn create(&self) -> TakeDuration<SkipDuration<Speed<Decoder<BufReader<File>>>>> {
+    fn new(&self) -> Decoder<BufReader<File>> { //opens and decodes song
         let file = File::open(&self.path).expect("Couldn't open file");
         let file = BufReader::new(file);
+        Decoder::new(file).expect("Couldn't decode")
+    }
 
-        let source = Decoder::new(file).expect("Couldn't decode");
-
-        let source = source
-            .speed(self.speed)
-            .skip_duration(Duration::from_secs_f32(self.start_time))
-            .take_duration(Duration::from_secs_f32(self.end_time-self.start_time));
-
-        source
+    fn clip(&self, speed: f32, start_time: f32, end_time: f32, loop_track: bool) -> impl Source<Item = i16> + Send{ //cleaned up return statement with claude
+        self.new()
+            .speed(speed)
+            .skip_duration(Duration::from_secs_f32(start_time))
+            .take_duration(Duration::from_secs_f32(end_time - start_time))
     }
 }
 
@@ -36,31 +30,19 @@ fn main() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).expect("Couldn't make sink");
     //Sink is like the audio player
-
-
+    
     //file path relative to Cargo.toml
     // let file = File::open(format!("music/{}.mp3", video_title)).expect("Couldn't open file");
-
-    //When I start on the sampler, I should have a song stuct but also have clip structs where I can save each clip 
-    let mut dtmlive: Song = Song{
+    let dtmlive: Song = Song{ //base song, can make modifications via clips
         path: String::from("music/dtmlive.mp3"),
-        speed: 1.3,
-        start_time: 0.0,
-        end_time: 10.0,
-        loop_track: false
     };
 
-    //messing with the song
-    // let source = source.take_duration(Duration::from_secs_f32(0.25));
-    // let source = source.fade_in(Duration::from_secs(4)).take_duration(Duration::from_secs(2)).repeat_infinite();
+    let clip1 = dtmlive.clip(1.5, 6.0, 12.0, false);
+    let clip2 = dtmlive.clip(0.80, 30.0, 33.0, false);
     
-    sink.append(dtmlive.create());
-
-    dtmlive.speed = 1.0;
-    dtmlive.start_time = 13.0;
-    dtmlive.end_time = 25.0;
-    sink.append(dtmlive.create());
-
+    sink.append(clip1);
+    sink.append(clip2);
+    
     sink.sleep_until_end();
     println!("Hello, world!");
 }
